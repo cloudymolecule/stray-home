830e8f8c1a45e6dde5a5619286a548bbf4d43cba
Cloudymolecule <baires82@gmail.com>
2021-09-05 08:12:24 -0400

    Add search and nav

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Nav.css'
+
+function Nav() {
+    return (
+        <div>
+            NAV
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Search.css'
+
+function Search() {
+    return (
+        <div>
+            SEARCH
+        </div>
+    )
+}
+
+export default Search
