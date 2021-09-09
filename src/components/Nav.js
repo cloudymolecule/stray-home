40e6a4b688c3c8b88fb34b4e56986a663921ede3
Cloudymolecule <baires82@gmail.com>
2021-09-09 09:18:30 -0400

    Add nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IconContext } from 'react-icons/lib'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import '../styles/Nav.css'
 
 function Nav() {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+    const closeMobileMenu = () => {
+        setClick(false)
+    }
+
     return (
-        <div>
-            NAV
-        </div>
+        <>
+            <IconContext.Provider value={{ color: '#000' }}>
+                <div className='nav'>
+                    <div className='nav-container container'>
+                        <Link onClick={closeMobileMenu} to='/' className='nav-logo'>
+                            <div className='nav-name'>Stray Home</div>
+                        </Link>
+                        <div onClick={handleClick} className='menu-icon'>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/'>HOME</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/cat-adoption'>CAT ADOPTION</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/dog-adoption'>DOG ADOPTION</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/rehome'>REHOME</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/shelters'>SHELTERS</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link onClick={closeMobileMenu} className='nav-link' to='/account'>ACCOUNT</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </IconContext.Provider>
+        </>
     )
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import '../styles/App.css'
 import Home from '../components/Home'
 import About from '../components/About'
 import Footer from '../components/Footer'
+import Nav from '../components/Nav'
 import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
