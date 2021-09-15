d513efad5cb3d9903af19e0713ba9498d6088d11
Cloudymolecule <baires82@gmail.com>
2021-09-15 09:56:17 -0400

    Update Home.js and css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ import '../styles/Home.css'
 function Home() {
     return (
         <div className='home-container'>
-            HOME
+            <div className='home-title-container'>
+                <h1 className='home-title'>WELCOME TO STRAY HOME</h1>
+                <h3 className='home-description'>YOUR ONE STOP PET ADOPTION AND REHOMING SITE</h3>
+            </div>
         </div>
     )
 }
