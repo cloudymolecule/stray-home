ba0ea99104ceeaa9d6e654b8f85263b35bef00d2
Cloudymolecule <baires82@gmail.com>
2021-09-15 09:50:19 -0400

    Add css and component routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,11 @@ import Home from '../components/Home'
 import About from '../components/About'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
+import Account from '../components/Account'
+import Cats from '../components/Cats'
+import Dogs from '../components/Dogs'
+import Rehome from '../components/Rehome'
+import Shelters from '../components/Shelters'
 import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
@@ -12,11 +17,11 @@ function App() {
       <Nav />
       <Switch>
         <Route path='/' exact component={Home} />
-        <Route path='/cat-adoption' exact component={About} />
-        <Route path='/dog-adoption' exact component={About} />
-        <Route path='/rehome' exact component={About} />
-        <Route path='/shelters' exact component={About} />
-        <Route path='/account' exact component={About} />
+        <Route path='/cat-adoption' exact component={Cats} />
+        <Route path='/dog-adoption' exact component={Dogs} />
+        <Route path='/rehome' exact component={Rehome} />
+        <Route path='/shelters' exact component={Shelters} />
+        <Route path='/account' exact component={Account} />
       </Switch>
       <Footer />
     </Router>
